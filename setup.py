86f3b627d874e017e1442f71f3aac7fa88aa2109
yoavweber <yoav.weber@code.berlin>
2022-06-15 20:50:25 +0200

    update setup

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(
     name='crypto_trade_helpers',
     version='1.1',
     packages=['crypto_utils'], # contains our actual code
-    author='chris',
-    author_email='chris@packagr.app',
     description='a random person generator',
     # scripts=['bin/make-person'], # the launcher script
     install_requires=["pytest","aio_pika"] # our external dependencies
