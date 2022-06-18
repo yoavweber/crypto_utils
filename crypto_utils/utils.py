0f24134a962d131c913348ad1b0340f1cfeef7aa
yoavweber <yoav.weber@code.berlin>
2022-06-18 19:49:11 +0200

    adding deserializer and parsing functions

--- crypto_utils/utils.py
+++ crypto_utils/utils.py
@@ -1,12 +1,27 @@
 import os
 from crypto_utils.types import *
+import requests as r
+import json 
 
 def is_development() -> bool:
     if os.getenv("APP_ENV") != "prod":
         return True
     return False
 
+def parse_deserializer(data) -> UserDict:
+    '''adding the id to the fields to easily create objects'''
+    new_data = data["fields"]
+    new_data["id"] = data["pk"]
+    return new_data
 
+
+
+def res_to_user_list(res: r.Response) -> List[UserDict]:
+    '''parsing backend response to list of user dict'''
+    users_string = res.json()
+    users_raw = json.loads(users_string)
+    users = list(map(lambda user: parse_deserializer(user), users_raw))
+    return users
 
 
 def get_user_market(user: User) -> Market_Type:
@@ -14,3 +29,6 @@ def get_user_market(user: User) -> Market_Type:
     if user_market == Market_Type.Futures.value:
         return Market_Type.Futures
     return Market_Type.Spot
+
+
+
--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     packages=['crypto_utils'], # contains our actual code
     description='a random person generator',
     # scripts=['bin/make-person'], # the launcher script
-    install_requires=["pytest","aio_pika"] # our external dependencies
+    install_requires=["pytest","aio_pika","requests"] # our external dependencies
 )
