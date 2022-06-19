3adf7b683d747ce99138ffcfbc7d4b141970523a
yoavweber <yoav.weber@code.berlin>
2022-06-19 19:22:55 +0200

    adding init to mocked events

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='crypto_trade_helpers',
-    version='1.3.3',
+    version='1.3.4',
     packages=['crypto_utils'], # contains our actual code
     description='a random person generator',
     # scripts=['bin/make-person'], # the launcher script
