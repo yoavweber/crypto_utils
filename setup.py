af5144223deade889e5cf92379c9b95b12811bff
yoavweber <yoav.weber@code.berlin>
2022-06-27 19:54:49 +0200

    fixing wrong package name

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='crypto_trade_helpers',
-    version='1.4.3',
+    version='1.4.5',
     packages=['crypto_utils'], # contains our actual code
     description='a random person generator',
     # scripts=['bin/make-person'], # the launcher script
-    install_requires=["pytest","aio_pika","requests","unicorn_fy","dotenv"] # our external dependencies
+    install_requires=["pytest","aio_pika","requests","unicorn_fy","python-dotenv"] # our external dependencies
 )
