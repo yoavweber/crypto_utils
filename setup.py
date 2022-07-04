84b7e68d2c4a3ab894706685ff33ecb0912c4ec0
yoavweber <yoav.weber@code.berlin>
2022-07-04 22:26:47 +0200

    adding typing-extensions dependencie

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='crypto_trade_helpers',
-    version='1.5.0',
+    version='1.5.1',
     packages=['crypto_utils'],
     description='a random person generator',
-    install_requires=["pytest","aio_pika","requests","unicorn_fy","python-dotenv"] # our external dependencies
+    install_requires=["pytest","aio_pika","requests","unicorn_fy","python-dotenv","typing-extensions"] # our external dependencies
 )
